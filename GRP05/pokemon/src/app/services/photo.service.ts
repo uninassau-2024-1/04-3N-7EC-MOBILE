d5e446f0152d6a52b5f0f253275e97345f87ed86
PedroLuizC <140963403+PedroLuizC@users.noreply.github.com>
2024-05-21 09:16:12 -0300

    Add files via upload

---
+++ GRP05/pokemon/src/app/services/photo.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    // Adicione aqui a lógica para lidar com o arquivo de imagem salvo, se necessário
+  }
+
+  private async savePicture(photo: any) {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: any) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64(blob: Blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+}
---
+++ GRP05/pokemon/src/app/services/shared.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  private pokemonList: any[] = [];
+
+  constructor() {}
+
+  addPokemon(pokemon: any) {
+    const existingPokemon = this.pokemonList.find(p => p.name === pokemon.name);
+    if (existingPokemon) {
+      existingPokemon.count += 1;
+    } else {
+      pokemon.victories = 0;
+      pokemon.defeats = 0;
+      pokemon.draws = 0;
+      this.pokemonList.push(pokemon);
+    }
+  }
+
+  getPokemonList() {
+    return this.pokemonList;
+  }
+
+  getLastPokemon() {
+    return this.pokemonList.length > 0 ? this.pokemonList[this.pokemonList.length - 1] : null;
+  }
+
+  updatePokemonStats(name: string, result: 'win' | 'lose' | 'draw') {
+    const pokemon = this.pokemonList.find(p => p.name === name);
+    if (pokemon) {
+      if (result === 'win') {
+        pokemon.victories += 1;
+      } else if (result === 'lose') {
+        pokemon.defeats += 1;
+      } else if (result === 'draw') {
+        pokemon.draws += 1;
+      }
+    }
+  }
+}
